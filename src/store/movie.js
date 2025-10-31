a3a154c2043dfe134e65b0744fa9d73eb2709faa
FE-t4330n <t4330n@gmail.com>
2025-10-31 16:22:26 +0900

    fix1

--- src/store/movie.js
+++ src/store/movie.js
@@ -18,31 +18,32 @@ export const searchMovies = async (page) => {
     store.state.movies = [];
     store.state.message = "";
   }
-
   try {
+    // const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`)
     const res = await fetch("/api/movie", {
       method: "POST",
-      body: body.stringify({
+      body: JSON.stringify({
         title: store.state.searchText,
         page,
       }),
     });
-    const { Search, totalResults, Response, Error } = await res.json();
+    const { Response, Search, totalResults, Error } = await res.json();
     if (Response === "True") {
       store.state.movies = [...store.state.movies, ...Search];
       store.state.pageMax = Math.ceil(Number(totalResults) / 10);
     } else {
       store.state.message = Error;
+      store.state.pageMax = 1;
     }
   } catch (error) {
-    console.log("searchMovies error: ", error);
+    console.log("searchMovies error:", error);
   } finally {
     store.state.loading = false;
   }
 };
-
 export const getMovieDetails = async (id) => {
   try {
+    // const res = await fetch(`https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`)
     const res = await fetch("/api/movie", {
       method: "POST",
       body: JSON.stringify({
